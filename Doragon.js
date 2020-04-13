b98e61e9ebb4b0a4c86e88c4f495443c5a371f27
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2020-04-13 12:03:03 +0900

    クラスモデリング

---
+++ Doragon.js
@@ -0,0 +1,49 @@
+class Doragon (name,HP,MP,buki,) {
+    this._name=name;
+    this._HP=HP;
+    this.=MP=MP;
+    let buki=buki;
+}
+
+.Yuusya{
+    name ='ヨシヒコ';
+    HP ='1000';
+    MP ='400';
+    buki='勇者の剣';    
+}
+
+Yuusya.kogeki('700ダメージを与えた');
+
+.Wizard{
+    name = 'ムラサキ';
+    HP ='900';
+    MP ='600';
+    buki ='魔法の杖';
+}
+
+Wizard.kogeki('400ダメージを与えた');
+Wizard.zyumon('ザキを唱えた');
+
+.Cleric{
+    name = 'ベネブ';
+    HP ='900';
+    MP ='700';
+    buki ='賢者の杖';
+}
+
+Cleric.kogeki('400ダメージを与えた');
+Cleric.zyumon('500回復');
+
+.Berseker{
+    name = 'ダンジョン';
+    HP ='900';
+    MP ='400';
+    buki ='剣士のつるぎ';
+}
+
+Berseker.kogeki('700ダメージを与えた');
+Berseker.hensin('獣化');
+
+class Doragon extends teki{
+    constructor
+}
