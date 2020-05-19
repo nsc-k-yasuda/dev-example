08e50e7ccc10583588b50854391b70ada3684aab
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2020-05-20 00:50:12 +0900

    TODOLIST

---
+++ TodoList.js
@@ -0,0 +1,39 @@
+//TODOリストを表す
+class TodoList extends BaseList {
+
+    constructor() {
+        super();
+    }
+
+    get count() {
+        return this._list.length;
+    }
+
+    addTodo(todo) {
+        super.addTodo(todo);
+    }
+
+    addAll(newList){
+        super.addAll(newList);
+    }
+
+    removeItem(todo) {
+        super.removeItem(todo);
+    }
+
+    //IDを使ってTODOを検索する
+    findItemById(itemId){
+        return this._list.find((item) => item.id == itemId);
+    }
+
+    //IDを使ってアイテムを削除する
+    removeItemById(itemId){
+        
+        let item = this.findItemById(itemId);
+
+        if(item) {
+            this.removeItem(item);
+        }
+
+    }
+}
